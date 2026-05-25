89a9628960553f59db355d1733cabd7b4796c3e4
DAVI NOGUEIRA DE SOUZA BARROSO RODRIGUES <41286020867@info.local>
2026-05-25 14:48:44 -0300

    Repositorio Criado

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+/*Autor: Grupo dk
+Programa: 17 - Projeto IoT
+Descrição: Projeto Final do Primeiro Semestre
+Data: 25/05/2026
+Versão:1.0
+*/
+
+void setup() 
+{
+
+}
+
+void loop() 
+{
+  
+}
+
